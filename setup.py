a9aa28bcc8cdd525871d19c3d52e60825324b44c
Emin Serin <eminn38@gmail.com>
2021-06-28 17:32:41 +0200

    - First Commit!

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='DeepConnectome',
+    version='0.1',
+    packages=['bin', 'docs', 'tests', 'deepconnectome', 'deepconnectome.io', 'deepconnectome.viz',
+              'deepconnectome.utils', 'deepconnectome.models', 'deepconnectome.evaluation',
+              'deepconnectome.preprocessing'],
+    url='',
+    license='GPL-3.0',
+    author='Emin Serin',
+    author_email='emin.serin@charite.de',
+    description='A python toolbox to make connectome-based predictions using BrainNetCNN. '
+)
